8c6b9ccd9cef5d8c2ff3aa28d250a98c7f6e4c96
Mitesh Patel <miteshpatel.tech@gmail.com>
2014-03-08 11:19:55 +0530

    Working Sample Maps

--- BookmarkPlaces/src/com/dexter/bookmarkplaces/screens/MapActivity.java
+++ BookmarkPlaces/src/com/dexter/bookmarkplaces/screens/MapActivity.java
@@ -4,15 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.dexter.bookmarkplaces.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends Activity {
 	  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
 	  static final LatLng KIEL = new LatLng(53.551, 9.993);
 	 private GoogleMap map;
+	 	 
 
+	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -21,20 +27,20 @@ public class MapActivity extends Activity {
 		   setContentView(R.layout.mapactivity);
 		    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 		        .getMap();
-//		    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-//		        .title("Hamburg"));
-//		    Marker kiel = map.addMarker(new MarkerOptions()
-//		        .position(KIEL)
-//		        .title("Kiel")
-//		        .snippet("Kiel is cool")
-//		        .icon(BitmapDescriptorFactory
-//		            .fromResource(R.drawable.ic_launcher)));
-//
-//		    // Move the camera instantly to hamburg with a zoom of 15.
-//		    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-//
-//		    // Zoom in, animating the camera.
-//		    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+		    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+		        .title("Hamburg"));
+		    Marker kiel = map.addMarker(new MarkerOptions()
+		        .position(KIEL)
+		        .title("Kiel")
+		        .snippet("Kiel is cool")
+		        .icon(BitmapDescriptorFactory
+		            .fromResource(R.drawable.ic_launcher)));
+
+		    // Move the camera instantly to hamburg with a zoom of 15.
+		    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+		    // Zoom in, animating the camera.
+		    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 	}
 	
 
